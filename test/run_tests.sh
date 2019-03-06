ebd6ccfe98cba5801c123fe9f57c1c2b564cc3f9
adam-dziedzic <adam.dziedzi@gmail.com>
2019-03-06 11:44:48 -0600

    Update run_tests.sh
    Remove files/dirs if exist.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,14 +1,15 @@
 # Cleanup
-rm -r tdb
-rm -r dbs
-rm -r temp
-rm -r videos_tests
+rm -rf tdb
+rm -rf dbs
+rm -rf temp
+rm -rf videos_tests
 mkdir dbs  # necessary for Descriptors
 mkdir temp # necessary for Videos
 mkdir videos_tests
 
 # Compile and run tests
-scons -j16
+n=`getconf _NPROCESSORS_ONLN`
+scons -j$n
 
-rm log.log
+rm -f log.log
 ./main 2> log.log
